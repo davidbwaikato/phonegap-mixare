85f711fbc626c8e0664ed30eeb178cdd0b004c77
David Bainbridge <davidb@cs.waikato.ac.nz>
2014-04-16 17:23:20 +1200

    Initial set of files to phonegap project, where plugin code is explicitly included

---
+++ phonegap-setup.bash
@@ -0,0 +1,8 @@
+export PATH=/cygdrive/c/Apps/Android/android-sdk/tools/:/cygdrive/c/Apps/Android/android-sdk/platform-tools:$PATH
+export PATH=~/research/packages/ant-1.9.2/bin/:$PATH
+
+
+export JAVA_HOME=/cygdrive/c/Program\ Files/Java/jdk1.8.0
+
+export PATH=$JAVA_HOME/bin:$PATH
+
